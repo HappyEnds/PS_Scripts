08a6540fc9c233452ee5251aad9a3f77016c19ae
Georgy <57131647+zZdiamontZz@users.noreply.github.com>
2019-10-29 15:51:40 +0300

    Update Add_new_attr_Value_by_using_3_strong_conditions.ps1

--- AD/Add_new_attr_Value_by_using_3_strong_conditions.ps1
+++ AD/Add_new_attr_Value_by_using_3_strong_conditions.ps1
@@ -1,8 +1,11 @@
-﻿$OUDN = Read-Host "Insert OU Distinguished Name For Search Users and Groups"
+﻿
+$OUDN = Read-Host "Insert OU Distinguished Name For Search Users and Groups"
 $GRPNamePart = Read-Host "Insert a part of Group name for search filtering"
+
 $AttrValue0 = Read-Host "Insert Attr Value for adding in first condition"
 $AttrValue1 = Read-Host "Insert Attr Value for adding in second condition"
 $AttrValue2 = Read-Host "Insert Attr Value for adding in third condition"
+
 $KeyAttrValue00 = "0"
 $KeyAttrValue01 = "1"
 
@@ -12,7 +15,7 @@ $UsersFromOU = Get-ADUser -Filter * -SearchBase $OUDN -Properties *
 $GroupsFromOU = Get-ADGroup -Filter {Name -like $GRPNamePart} -SearchBase $OUDN
 
 foreach ($User in $UsersFromOU) {
-
+#User have surname 0, and he is member of some group in OU where his account stored
    if ($User.Surname -eq $KeyAttrValue00) {
 
       Foreach ($Group in $GroupsFromOU) {
@@ -20,7 +23,7 @@ foreach ($User in $UsersFromOU) {
          $GroupMember = Get-ADGroupMember -Identity $Group.Name | Where-Object {$_.SamAccountName -eq $User.SamAccountName}
 
          if ($GroupMember -ne $null) {
-
+         #Set City Attrr using your first condition entry
             Set-ADUser -Identity $User.SamAccountName -City $AttrValue0
 
            }
@@ -34,18 +37,19 @@ foreach ($User in $UsersFromOU) {
          $GroupMember = Get-ADGroupMember -Identity $Group.Name | Where-Object {$_.SamAccountName -eq $User.SamAccountName}
 
          if ($GroupMember -ne $null) {
-
+         #Set City Attrr using your second condition entry
             Set-ADUser -Identity $User.SamAccountName -City $AttrValue1
 
            }
        }
    }
-   
+   #In all other cases
    else {
-
+   #Set City Attrr using your third condition entry
    Set-ADUser -Identity $User.SamAccountName -City $AttrValue2
 
    }
-
+   #Clear variables need for big arrays of data and for sure that in every step of cicle you have right data
    Clear-Variable User,Group,GroupMember
+   #Happy End
 }
