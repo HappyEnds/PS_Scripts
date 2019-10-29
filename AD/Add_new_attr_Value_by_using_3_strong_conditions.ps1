aa249f0ac212bae21ae17f14334fe8b0a7532999
Georgy <57131647+zZdiamontZz@users.noreply.github.com>
2019-10-29 15:42:21 +0300

    Create Add_new_attr_Value_by_using_3_strong_conditions.ps1
    In my case, i need to change the city attr
    if:
    User have surname 0, and he is member of some group  in OU where his account stored. I Need to change his city to 0
    or:
    User have surname 1, and he is member of some group  in OU where his account stored. I Need to change his city to 1
    else:
    User surname not equal 0 or 1 and he isn`t member of list of groups.  I Need to change his city to 2
    
    But this is universal scenario for tasks with similar requirements

---
+++ AD/Add_new_attr_Value_by_using_3_strong_conditions.ps1
@@ -0,0 +1,51 @@
+﻿$OUDN = Read-Host "Insert OU Distinguished Name For Search Users and Groups"
+$GRPNamePart = Read-Host "Insert a part of Group name for search filtering"
+$AttrValue0 = Read-Host "Insert Attr Value for adding in first condition"
+$AttrValue1 = Read-Host "Insert Attr Value for adding in second condition"
+$AttrValue2 = Read-Host "Insert Attr Value for adding in third condition"
+$KeyAttrValue00 = "0"
+$KeyAttrValue01 = "1"
+
+$GRPNamePart = $GRPNamePart + "*"
+
+$UsersFromOU = Get-ADUser -Filter * -SearchBase $OUDN -Properties *
+$GroupsFromOU = Get-ADGroup -Filter {Name -like $GRPNamePart} -SearchBase $OUDN
+
+foreach ($User in $UsersFromOU) {
+
+   if ($User.Surname -eq $KeyAttrValue00) {
+
+      Foreach ($Group in $GroupsFromOU) {
+
+         $GroupMember = Get-ADGroupMember -Identity $Group.Name | Where-Object {$_.SamAccountName -eq $User.SamAccountName}
+
+         if ($GroupMember -ne $null) {
+
+            Set-ADUser -Identity $User.SamAccountName -City $AttrValue0
+
+           }
+       }
+   }
+
+   elseif ($User.Surname -eq $KeyAttrValue01) {
+   
+      foreach ($Group in $GroupsFromOU) {
+      
+         $GroupMember = Get-ADGroupMember -Identity $Group.Name | Where-Object {$_.SamAccountName -eq $User.SamAccountName}
+
+         if ($GroupMember -ne $null) {
+
+            Set-ADUser -Identity $User.SamAccountName -City $AttrValue1
+
+           }
+       }
+   }
+   
+   else {
+
+   Set-ADUser -Identity $User.SamAccountName -City $AttrValue2
+
+   }
+
+   Clear-Variable User,Group,GroupMember
+}
