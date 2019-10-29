50336d060e120eabddb69a34e3274b0866288fd0
Georgy <57131647+zZdiamontZz@users.noreply.github.com>
2019-10-29 15:27:47 +0300

    Create PWD_Generator.ps1

---
+++ Windows/PWD_Generator.ps1
@@ -0,0 +1,17 @@
+﻿#Pass exit count
+$PassCount = 65
+#Create txt file for password storing
+New-Item -Path c:\temp -Name 2.txt -ItemType File
+#Cicle
+for ( $i = 1 ; $i -le $PassCount ; $i++ ) {
+#Store all using symbls to var, assign minimum one number, create var with symbs
+$chars = "abcdefghijkmnopqrstuvwxyzABCEFGHJKLMNPQRSTUVWXYZ123456789!#%?".ToCharArray()
+$symb = "123456789!#%?".ToCharArray() | Get-Random
+$newPassword=""
+1..9 | ForEach {  $newPassword += $chars | Get-Random }
+$newPassword = $newPassword + $symb
+$word = $newPassword
+#Add password string to file
+Add-Content -Path c:\temp\2.txt -Value $word
+#Happy End
+}
