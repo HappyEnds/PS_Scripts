2fbd40bcd6129968903eeca95f6e6f1bfd4bf804
Georgy <57131647+zZdiamontZz@users.noreply.github.com>
2019-10-29 14:53:47 +0300

    Create List_Of_Hot_Fixes_And_Installed_Software.ps1

---
+++ List_Of_Hot_Fixes_And_Installed_Software.ps1
@@ -0,0 +1,8 @@
+﻿
+#Path of files
+$HFPath = "c:\temp\Updates.csv"
+$SoftPath = "c:\temp\Soft.csv"
+#Get and write all hotfixes and parameters to csv file
+Get-HotFix | Export-Csv -Path $HFPath -Delimiter ";" -Encoding UTF8
+#Get and write all installed software products and parameters to csv file
+Get-WmiObject -Class win32_product | Export-Csv -Path $SoftPath -Delimiter ";" -Encoding UTF8
