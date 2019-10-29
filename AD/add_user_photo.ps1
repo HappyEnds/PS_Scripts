6c67797330347f5ec88d75474984319718c39951
Georgy <57131647+zZdiamontZz@users.noreply.github.com>
2019-10-29 13:07:26 +0300

    Create add_user_photo.ps1

---
+++ AD/add_user_photo.ps1
@@ -0,0 +1,15 @@
+﻿#Для того чтобы этот скрипт сработал, название файлов фотографий должно совпадать с атрибутом Samaccountname пользователей
+#Путь к файлам фотографий
+$PhotosPath = "C:\Temp"
+#Получение списка всех файлов
+$PhotosFilePath = Get-ChildItem -Path $PhotosPath
+#Цикл для каждого объекта фотографии
+Foreach ($UserPhoto in $PhotosFilePath) {
+#Перекодируем файл фотографии в понятный АД
+$photo = [byte[]](Get-Content $UserPhoto -Encoding byte)
+#Забираем из имени файла имя пользователя
+$User = $UserPhoto.Name -replace $filesformat,''
+#Задаем фотографию пользователю
+Set-ADUser -Identity $User -Add @{thumbnailPhoto=$photo}
+#Счастливый конец
+}
